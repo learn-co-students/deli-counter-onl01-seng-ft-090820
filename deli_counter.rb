388c1decde898f287fff81fef9fcbbec55831f37
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-16 18:22:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli) 
+  katz_deli = [] 
+  counter = 1
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length >=0 
+    puts "The line is currently: #{counter} . #{katz_deli.join(", ")}"
+    counter += 1
+  else
+    katz_deli
+  end
+end
+
