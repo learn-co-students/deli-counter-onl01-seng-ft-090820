e74270e1a211773235216c6748e5b58f25db7a3a
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-17 16:21:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+
+def line(katz_deli)
+  if katz_deli == [ ]
+  puts "The line is currently empty."
+  elsif line(katz_deli) == [ ]
+  return "The line is currently: #{ }."
+  else
+  end 
+end 
+
+def take_a_number(katz_deli, name)
+ katz_deli.push 
+  return katz_deli
+end
+  
+def now_serving(katz_deli)
+  
+     
+   
+end 
+
+
+
+
+
+
+
