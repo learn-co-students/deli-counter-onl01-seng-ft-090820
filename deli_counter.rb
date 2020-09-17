0a88a1d59f08fa7460a03b0a118fd508c3d227fa
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-17 17:07:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@ require "pry"
 
 def line(katz_deli)
   if katz_deli == [ ]
-  puts "The line is currently empty."
-  elsif line(katz_deli) == [ ]
-  return "The line is currently: #{ }."
-  else
+    puts ("The line is currently empty.")
+  else 
+    currently = "The line is currently:"
+    
+    puts "The line is currently: #{ }."
   end 
 end 
 
