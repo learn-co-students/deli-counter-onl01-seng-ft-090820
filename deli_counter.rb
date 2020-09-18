ec8d20b022b76f965320fc4918474901022f8fe2
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-18 00:47:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,27 +3,21 @@ require "pry"
 def line(katz_deli)
   if katz_deli == [ ]
     puts ("The line is currently empty.")
-  else 
+  else
     currently = "The line is currently:"
-    
-    puts "The line is currently: #{ }."
-  end 
+    katz_deli.each.with_index(1) do |customer, index|
+      currently += " #{index}. #{customer}"  
+    end
+    puts currently 
+  end
 end 
+ 
 
 def take_a_number(katz_deli, name)
- katz_deli.push 
-  return katz_deli
+  katz_deli.push do |name, index|
+    puts "Welcome #{name}. You are number #{index} in line."
+    binding.pry
 end
   
-def now_serving(katz_deli)
-  
-     
-   
-end 
-
-
-
-
-
-
-
+# def now_serving(katz_deli)
+# end 
