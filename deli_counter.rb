32c23c8cae5c07aa249922bfef06fc57472d69dd
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 16:12:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,38 @@
-katz_deli = []
+katz_deli = [] 
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  position = katz_deli.length
-  puts "Welcome, #{name}! You are number #{position} in line."
-  position - 1
-end
-
-def now_serving(katz_deli)
-  puts "Now serving #{katz_deli[0]}!"
-  katz_deli.shift
-end
-
-def current_place(katz_deli, name)
-  unless katz_deli.size == 0
-    puts "We'll be right with you, #{name}. You are number #{katz_deli.index(name) + 1} in line."
-  if katz_deli.size < 0
-    puts "There is nobody in line"
-    end
-end
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
 
 take_a_number(katz_deli, "Sandy")
 take_a_number(katz_deli, "Mandy")
 take_a_number(katz_deli, "Handy")
 
-while katz_deli.length > 0
-  now_serving(katz_deli)
-  sleep(2)
-  current_place(katz_deli, "Handy")
-  end
-end
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
+ 
+line(katz_deli)
+
+
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
+
+now_serving(katz_deli)
