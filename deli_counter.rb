46750d0ff02ae4581b8acdb01ec881238614faa2
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 15:57:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  position = katz_deli.length
+  puts "Welcome, #{name}! You are number #{position} in line."
+  position - 1
+end
+
+def now_serving(katz_deli)
+  puts "Now serving #{katz_deli[0]}!"
+  katz_deli.shift
+end
+
+def current_place(katz_deli, name)
+  unless katz_deli.size == 0
+    puts "We'll be right with you, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  end
+end
+
+take_a_number(katz_deli, "Sandy")
+take_a_number(katz_deli, "Mandy")
+take_a_number(katz_deli, "Handy")
+
+while katz_deli.length > 0
+  now_serving(katz_deli)
+  sleep(2)
+  current_place(katz_deli, "Handy")
+end
